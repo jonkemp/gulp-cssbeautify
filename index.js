c46a6e06f0b09853971d7adc266513375cd2b2ff
Jonathan Kemp <kempdogg@gmail.com>
2014-03-29 16:41:05 -0400

    Minor changes

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
+'use strict';
 var gutil = require('gulp-util');
 var through = require('through2');
 var cssbeautify = require('cssbeautify');
 
 module.exports = function(opt){
     return through.obj(function (file, enc, cb) {
-        if (!opt) {
-            opt = {};
-        }
-
-        if (file.isNull()) {
-            this.push(file);
-            return cb();
-        }
+        opt = opt || {};
 
         if (file.isStream()) {
             this.emit('error', new gutil.PluginError('gulp-cssbeautify', 'Streaming not supported'));
